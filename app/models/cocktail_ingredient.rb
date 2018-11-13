63baa7eb98a2c462d24f1d626836e9feb0a222c8
Robert Schulte <schulte.robertw@outlook.com>
2018-11-13 17:06:16 -0600

    flash notices for new and edit

--- app/models/cocktail_ingredient.rb
+++ app/models/cocktail_ingredient.rb
@@ -2,5 +2,5 @@ class CocktailIngredient < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates_uniqueness_of :ingredient_id, scope: :cocktail_id
+  # validates_uniqueness_of :ingredient_id, scope: :cocktail_id
 end

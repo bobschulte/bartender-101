dbb5197450fa2392fffe2b796e85a0981e5d0beb
Robert Schulte <schulte.robertw@outlook.com>
2018-11-14 14:21:18 -0600

    built user controller & views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,27 @@
 class UsersController < ApplicationController
     before_action :define_current_user
-    
+
     def create
         user = User.create(user_params)
         session[:current_user_id] = user.id
+        redirect_to user_path(user)
         # redirect_to '/login'
     end
-    
+
     def index
         @users = User.all
     end
-    
+
     def update
         @user.update(user_params)
         redirect_to @user
     end
-    
+
     # def destroy
     #     @user.destroy
     #     redirect_to users_path
     # end
-    
+
     def define_current_user
         if params[:id]
             @user = User.find(params[:id])
@@ -28,7 +29,7 @@ class UsersController < ApplicationController
             @user = User.new
         end
     end
-    
+
     def user_params
         params.require(:user).permit(:first_name, :last_name, :username, :password)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end

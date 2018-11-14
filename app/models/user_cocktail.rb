850f46720079bd0e65d5b6afe3ee125f63613010
jordan-laird <jordanmack.laird@gmail.com>
2018-11-14 17:34:58 -0600

    removed usercocktails

--- app/models/user_cocktail.rb
+++
@@ -1,4 +0,0 @@
-class UserCocktail < ApplicationRecord
-    belongs_to :user
-    belongs_to :cocktail
-end
--- test/controllers/user_cocktails_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserCocktailsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
